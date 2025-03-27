f9e59a80777af7fe28fa0e5c9951a4ebfe0365dd
Jamie Drinkell <94254558+M0JXD@users.noreply.github.com>
2025-03-27 21:15:59 +0000

    Fix capitalised keybindings while retaining other fixes

--- core/keys.lua
+++ core/keys.lua
@@ -152,8 +152,12 @@ events.connect(events.KEY, function(code, modifiers)
 	local key = code >= 32 and code < 256 and string.char(code) or M.KEYSYMS[code]
 	if not key then return end
 	if QT and not shift and code < 256 then key = key:lower() end -- Qt always give uppercase codes
-	-- Since printable characters are uppercased, disable shift. (Except on Haiku)
-	if (not HAIKU) then if shift and code >= 32 and code < 256 then shift = false end end
+	-- Since printable characters are uppercased, disable shift. (Disable for non-characters on Haiku)
+	if (not HAIKU) then 
+		if shift and code >= 32 and code < 256 then shift = false end 
+	else
+		if shift and ((code >= 48 and code < 56) or (code >= 65 and code < 90) or (code >= 97 and code < 122)) then shift = false end 
+	end
 	-- For composed keys on macOS, ignore alt.
 	if (OSX and not CURSES) and alt and code < 256 then alt = false end
 	return events.emit(events.KEYPRESS,
